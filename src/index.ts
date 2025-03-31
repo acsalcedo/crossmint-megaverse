bdb3fe2fb8b861d2d565823c880d3c400aee51ec
Andrea Salcedo <andreacsalcedo@gmail.com>
2025-03-31 13:09:52 +0200

    Add first phase solution

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-export const main = (): string => 'Hello World';
+import { CrossmintAPIService } from './services/crossmintAPIService';
 
-console.log(main());
+export const main = async () => { 
+  const crossmintAPI = new CrossmintAPIService();
+
+  const megaverse = await crossmintAPI.getGoalMap();
+
+  for (let i = 0; i < megaverse.length;  i++) {
+    const row =  megaverse[i];
+    for (let j = 0; j < row.length; j++) {
+      const tile = row[j];
+
+      if (tile === 'POLYANET') {
+        await crossmintAPI.createPolyanet({ row: i, column: j});
+      }
+    }
+  }
+};
+
+main();
+
