1d73e5c4f3839f3b65254ab6b19708bb9066f83e
Andrea Salcedo <andreacsalcedo@gmail.com>
2025-04-02 14:39:00 +0200

    Improve main function and check if adjacent objects are a planet

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,51 @@
-import { Color, CrossmintAPIService, Direction } from './services/crossmintAPIService';
+import { Megaverse, MegaverseObject, MegaverseObjectType } from './megaverse.entity';
+import { CrossmintAPIService } from './services/crossmint-api.service';
+import { CreateMegaverseObjectInput } from './services/crossmint-api.types';
 
-const directionMap: Record<string, Direction> = {
-  'UP_COMETH': 'up',
-  'DOWN_COMETH': 'down',
-  'LEFT_COMETH': 'left',
-  'RIGHT_COMETH': 'right'
+
+const mapToCreateInput = (obj: MegaverseObject, row: number, column: number): CreateMegaverseObjectInput => {
+  switch (obj.type) {
+  case MegaverseObjectType.POLYANET:
+    return { row, column, type: MegaverseObjectType.POLYANET };
+  case MegaverseObjectType.COMETH:
+    return { row, column, type: MegaverseObjectType.COMETH, direction: obj.direction };
+  case MegaverseObjectType.SOLOON:
+    return { row, column, type: MegaverseObjectType.SOLOON, color: obj.color };
+  default:
+    return null;
+  }
 };
 
-const colorMap: Record<string, Color> = {
-  'BLUE_SOLOON': 'blue',
-  'RED_SOLOON': 'red',
-  'PURPLE_SOLOON': 'purple',
-  'WHITE_SOLOON': 'white',
+// Checks if a megaverse object can be created
+const canBeCreated = (obj: MegaverseObject, megaverse: Megaverse, row: number, column: number): boolean => {
+  if (obj.type !== MegaverseObjectType.SOLOON) {
+    return true;
+  }
+
+  // Left, right, up, down
+  const coordinates = [[0,-1], [0,1], [-1,0], [1,0]];
+
+  // If the object is a soloon, then we need to check if one of the adjacent objects is a polyanet.
+  return coordinates.some(([i, j]) => {
+    const adjacentObject = megaverse[row + i][column + j];
+
+    return adjacentObject.type === MegaverseObjectType.POLYANET;
+  });
 };
 
 export const main = async () => { 
   const crossmintAPI = new CrossmintAPIService();
-
   const megaverse = await crossmintAPI.getGoalMap();
 
-  for (let i = 0; i < megaverse.length;  i++) {
-    const row =  megaverse[i];
-    for (let j = 0; j < row.length; j++) {
-      const tile = row[j];
-
-      if (tile === 'POLYANET') {
-        await crossmintAPI.createPolyanet({ row: i, column: j });
-
-      } else if (tile.includes('COMETH')) {
-        const direction = directionMap[tile];
-
-        if (!direction) {
-          // handle error
-        }
-
-        await crossmintAPI.createCometh({ row: i, column: j, direction });
-
-      } else if (tile.includes('SOLOON')) {
-        const color = colorMap[tile];
-
-        if (!color) {
-          // handle error
-        }
-
-        await crossmintAPI.createSoloon({ row: i, column: j, color });
+  for (const [i, row] of megaverse.entries()) {
+    for (const [j, obj] of row.entries()) {
+      const createInput = mapToCreateInput(obj, i, j);
+      
+      if (createInput && canBeCreated(obj, megaverse, i, j)) {
+        await crossmintAPI.createMegaverseObject(createInput);
       }
     }
   }
 };
 
-// TODO
-// convert strings into objects that can be easily processed
-// separate typing into another file
-// adding restriction to soloons in my code instead of just assuming that the given goal is good
 main();
-
