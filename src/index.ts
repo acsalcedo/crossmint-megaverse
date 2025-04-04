61c290904740b3ec2b08440645fd1a3ca261f041
Andrea Salcedo <andreacsalcedo@gmail.com>
2025-04-04 12:02:17 +0200

    Move main function logic into megaverse class

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,8 @@
-import { Megaverse, MegaverseObject, MegaverseObjectType } from './megaverse.entity';
+import { MegaverseService } from './services/megaverse.service';
 import { CrossmintAPIService } from './services/crossmint-api.service';
-import { CreateMegaverseObjectInput } from './services/crossmint-api.types';
 
+if (process.env.NODE_ENV !== 'test') {
+  const megaverse = new MegaverseService(new CrossmintAPIService());
 
-const mapToCreateInput = (obj: MegaverseObject, row: number, column: number): CreateMegaverseObjectInput => {
-  switch (obj.type) {
-  case MegaverseObjectType.POLYANET:
-    return { row, column, type: MegaverseObjectType.POLYANET };
-  case MegaverseObjectType.COMETH:
-    return { row, column, type: MegaverseObjectType.COMETH, direction: obj.direction };
-  case MegaverseObjectType.SOLOON:
-    return { row, column, type: MegaverseObjectType.SOLOON, color: obj.color };
-  default:
-    return null;
-  }
-};
-
-// Checks if a megaverse object can be created
-const canBeCreated = (obj: MegaverseObject, megaverse: Megaverse, row: number, column: number): boolean => {
-  if (obj.type !== MegaverseObjectType.SOLOON) {
-    return true;
-  }
-
-  // Left, right, up, down
-  const coordinates = [[0,-1], [0,1], [-1,0], [1,0]];
-
-  // If the object is a soloon, then we need to check if one of the adjacent objects is a polyanet.
-  return coordinates.some(([i, j]) => {
-    const adjacentObject = megaverse[row + i][column + j];
-
-    return adjacentObject.type === MegaverseObjectType.POLYANET;
-  });
-};
-
-export const main = async () => { 
-  const crossmintAPI = new CrossmintAPIService();
-  const megaverse = await crossmintAPI.getGoalMap();
-
-  for (const [i, row] of megaverse.entries()) {
-    for (const [j, obj] of row.entries()) {
-      const createInput = mapToCreateInput(obj, i, j);
-      
-      if (createInput && canBeCreated(obj, megaverse, i, j)) {
-        await crossmintAPI.createMegaverseObject(createInput);
-      }
-    }
-  }
-};
-
-main();
+  megaverse.createMegaverse().catch((error) => console.error(error));
+}
---
+++ src/services/megaverse.service.ts
@@ -0,0 +1,50 @@
+import { Megaverse, MegaverseObject, MegaverseObjectType } from '../megaverse.entity';
+import { CreateMegaverseObjectInput, MegaverseAPIService } from './crossmint-api.types';
+
+export class MegaverseService {
+  constructor(private megaverseAPI: MegaverseAPIService) {}
+
+  async createMegaverse() {
+    const megaverse = await this.megaverseAPI.getGoalMap();
+    
+    for (const [i, row] of megaverse.entries()) {
+      for (const [j, obj] of row.entries()) {
+        const createInput = this.mapToCreateInput(obj, i, j);
+          
+        if (createInput && this.canBeCreated(obj, megaverse, i, j)) {
+          await this.megaverseAPI.createMegaverseObject(createInput);
+        }
+      }
+    }
+  }
+
+  private mapToCreateInput(obj: MegaverseObject, row: number, column: number): CreateMegaverseObjectInput  {
+    switch (obj.type) {
+    case MegaverseObjectType.POLYANET:
+      return { row, column, type: MegaverseObjectType.POLYANET };
+    case MegaverseObjectType.COMETH:
+      return { row, column, type: MegaverseObjectType.COMETH, direction: obj.direction };
+    case MegaverseObjectType.SOLOON:
+      return { row, column, type: MegaverseObjectType.SOLOON, color: obj.color };
+    default:
+      return null;
+    }
+  }
+  
+  // Checks if a megaverse object can be created
+  private canBeCreated(obj: MegaverseObject, megaverse: Megaverse, row: number, column: number): boolean {
+    if (obj.type !== MegaverseObjectType.SOLOON) {
+      return true;
+    }
+  
+    // Left, right, up, down
+    const coordinates = [[0,-1], [0,1], [-1,0], [1,0]];
+  
+    // If the object is a soloon, then we need to check if one of the adjacent objects is a polyanet.
+    return coordinates.some(([i, j]) => {
+      const adjacentObject = megaverse[row + i]?.[column + j];
+  
+      return adjacentObject?.type === MegaverseObjectType.POLYANET;
+    });
+  }
+}
